1792b5d6aeec22080ce6152b7fc4283c147e5c0a
ctsu <chente.su@gmail.com>
2025-06-07 19:27:35 +0800

    程式結構化bug修正2.6

--- modules/app.js
+++ modules/app.js
@@ -1,6 +1,7 @@
 // 導入其他模組
 import { setupEventListeners } from './events.js';
 import { showSwipeHint } from './ui.js';
+import './questions.js'; // 確保 questions.js 被載入
 
 function initializeApp() {
     console.log('應用程式初始化...');
